b5a83a6d8e5f939510ca833a4501dca5833aec73
xxkinaxx <ekopandu088@gmail.com>
2024-05-30 13:56:49 +0700

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\TodoListController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +15,12 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::middleware('auth')->group(function() {
     Route::resource('/todo', TodoListController::class)->except('show', 'create', 'edit');
+});
+
+// Route::Artisan Call
+Route::get('/artisan-call', function(){
+    Artisan::call('storage:link'); // storage link
+    Artisan::call('route:clear'); // route clear
+    Artisan::call('config:clear'); // config clear
+    return 'success';
 });
